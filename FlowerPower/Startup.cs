34465de65f71228d1e66499e12fdcf531b61b571
Luuk Hamming <luuk.hamming@gmail.com>
2018-10-08 08:37:56 +0200

    Added the project

---
+++ FlowerPower/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FlowerPower.Startup))]
+namespace FlowerPower
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
