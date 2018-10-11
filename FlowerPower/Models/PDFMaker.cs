dbb7bc7689a95371b685595c5b8291375f307ede
stefan <stefan.lam@hotmail.com>
2018-10-11 08:36:43 +0200

    PDF verder dan eerst

---
+++ FlowerPower/Models/PDFMaker.cs
@@ -0,0 +1,376 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using FlowerPower.Models;
+using FlowerPower;
+
+namespace FlowerPower.Models
+{
+    public class PDFMaker
+    {
+        int _totalColumn = 4;
+        Document _document;
+        Font _fontstyle;
+        PdfPTable _pdfTable = new PdfPTable(4);
+        PdfPCell _pdfCell;
+        MemoryStream _memoryStream = new MemoryStream();
+        List<bestelling> _bestellingen = new List<bestelling>();
+
+        public byte[] PreparePDF(List<bestelling> bestellingen)
+        {
+            _bestellingen = bestellingen;
+
+            _document = new Document(PageSize.A4, 0f, 0f, 0f, 0f);
+            _document.SetPageSize(PageSize.A4);
+            _document.SetMargins(20f, 20f, 20f, 20f);
+            _pdfTable.WidthPercentage = 100;
+            _pdfTable.HorizontalAlignment = Element.ALIGN_CENTER;
+            _fontstyle = FontFactory.GetFont("Tahoma", 8f, 1);
+            PdfWriter.GetInstance(_document, _memoryStream);
+            _document.Open();
+            _pdfTable.SetWidths(new float[] { 30f, 90f, 50f, 100f });
+
+            this.ReportHeader();
+            this.ReportBody();
+            _pdfTable.HeaderRows = 2;
+            _document.Add(_pdfTable);
+            _document.Close();
+            return _memoryStream.ToArray();
+        }
+
+        private void ReportHeader()
+        {
+            _fontstyle = FontFactory.GetFont("Tahoma", 11f, 1);
+            _pdfCell = new PdfPCell(new Phrase("Aan:", _fontstyle));
+            _pdfCell.Colspan = 3;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfCell = new PdfPCell(new Phrase("Van:", _fontstyle));
+            _pdfCell.Colspan = 1;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _fontstyle = FontFactory.GetFont("Tahoma", 11f, 0);
+            _pdfCell = new PdfPCell(new Phrase("Marietje Kwakman", _fontstyle));
+            _pdfCell.Colspan = 3;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfCell = new PdfPCell(new Phrase("Flower Power", _fontstyle));
+            _pdfCell.Colspan = 1;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _pdfCell = new PdfPCell(new Phrase("Piet Hein laan 10", _fontstyle));
+            _pdfCell.Colspan = 3;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfCell = new PdfPCell(new Phrase("Beekzicht 21", _fontstyle));
+            _pdfCell.Colspan = 1;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _pdfCell = new PdfPCell(new Phrase("2145 KL Amsterdam", _fontstyle));
+            _pdfCell.Colspan = 3;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfCell = new PdfPCell(new Phrase("1246 HD Amsterdam", _fontstyle));
+            _pdfCell.Colspan = 1;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _pdfCell = new PdfPCell(new Phrase("Tel. 06 75223364", _fontstyle));
+            _pdfCell.Colspan = 3;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfCell = new PdfPCell(new Phrase("Tel. 021 1234567", _fontstyle));
+            _pdfCell.Colspan = 1;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _pdfCell = new PdfPCell(new Phrase("", _fontstyle));
+            _pdfCell.Colspan = 3;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfCell = new PdfPCell(new Phrase("Web: www.powerflower.nl", _fontstyle));
+            _pdfCell.Colspan = 1;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _pdfCell = new PdfPCell(new Phrase("", _fontstyle));
+            _pdfCell.Colspan = 3;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfCell = new PdfPCell(new Phrase("Mail: info@powerflower.nl", _fontstyle));
+            _pdfCell.Colspan = 1;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _pdfCell = new PdfPCell(new Phrase("\n", _fontstyle));
+            _pdfCell.Colspan = 4;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _pdfCell = new PdfPCell(new Phrase("\n", _fontstyle));
+            _pdfCell.Colspan = 4;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _pdfCell = new PdfPCell(new Phrase("\n", _fontstyle));
+            _pdfCell.Colspan = 4;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _fontstyle = FontFactory.GetFont("Tahoma", 11f, 1);
+            _pdfCell = new PdfPCell(new Phrase("Factuurgegevens", _fontstyle));
+            _pdfCell.Colspan = 4;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _fontstyle = FontFactory.GetFont("Tahoma", 11f, 0);
+            _pdfCell = new PdfPCell(new Phrase("Factuurnummer: 1002", _fontstyle));
+            _pdfCell.Colspan = 4;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _pdfCell = new PdfPCell(new Phrase("Factuurdatum: " + DateTime.Now, _fontstyle));
+            _pdfCell.Colspan = 4;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _pdfCell = new PdfPCell(new Phrase("Afhaaldatum: " + DateTime.Now, _fontstyle));
+            _pdfCell.Colspan = 4;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _pdfCell = new PdfPCell(new Phrase("\n", _fontstyle));
+            _pdfCell.Colspan = 4;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _pdfCell = new PdfPCell(new Phrase("\n", _fontstyle));
+            _pdfCell.Colspan = 4;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _pdfCell = new PdfPCell(new Phrase("\n", _fontstyle));
+            _pdfCell.Colspan = 4;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+        }
+
+        private void ReportBody()
+        {
+            // Table header
+            _fontstyle = FontFactory.GetFont("Tahoma", 8f, 1);
+            _pdfCell = new PdfPCell(new Phrase("Aantal", _fontstyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.LIGHT_GRAY;
+            _pdfTable.AddCell(_pdfCell);
+
+            _pdfCell = new PdfPCell(new Phrase("Naam boeket", _fontstyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.LIGHT_GRAY;
+            _pdfTable.AddCell(_pdfCell);
+
+            _pdfCell = new PdfPCell(new Phrase("Prijs per stuk", _fontstyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.LIGHT_GRAY;
+            _pdfTable.AddCell(_pdfCell);
+
+            _pdfCell = new PdfPCell(new Phrase("Subtotaal", _fontstyle));
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+            _pdfCell.BackgroundColor = BaseColor.LIGHT_GRAY;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+
+            // Table body
+            _fontstyle = FontFactory.GetFont("Tahoma", 8f, 0);
+            foreach (bestelling bestelling in _bestellingen)
+            {
+                _pdfCell = new PdfPCell(new Phrase("TestAantal", _fontstyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.WHITE;
+                _pdfTable.AddCell(_pdfCell);
+
+                _pdfCell = new PdfPCell(new Phrase("TestNaam", _fontstyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.WHITE;
+                _pdfTable.AddCell(_pdfCell);
+
+                _pdfCell = new PdfPCell(new Phrase("TestPrijs", _fontstyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.WHITE;
+                _pdfTable.AddCell(_pdfCell);
+
+                _pdfCell = new PdfPCell(new Phrase("TestSubTotaal", _fontstyle));
+                _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+                _pdfCell.VerticalAlignment = Element.ALIGN_MIDDLE;
+                _pdfCell.BackgroundColor = BaseColor.WHITE;
+                _pdfTable.AddCell(_pdfCell);
+
+                _pdfTable.CompleteRow();
+            }
+
+
+            _fontstyle = FontFactory.GetFont("Tahoma", 8f, 1);
+            _pdfCell = new PdfPCell(new Phrase("Totaal excl. BTW:", _fontstyle));
+            _pdfCell.Colspan = 3;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _fontstyle = FontFactory.GetFont("Tahoma", 8f, 0);
+            _pdfCell = new PdfPCell(new Phrase("TestTotaal", _fontstyle));
+            _pdfCell.Colspan = 1;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _fontstyle = FontFactory.GetFont("Tahoma", 8f, 1);
+            _pdfCell = new PdfPCell(new Phrase("21% BTW: ", _fontstyle));
+            _pdfCell.Colspan = 3;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _fontstyle = FontFactory.GetFont("Tahoma", 8f, 0);
+            _pdfCell = new PdfPCell(new Phrase("TestBTW", _fontstyle));
+            _pdfCell.Colspan = 1;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _fontstyle = FontFactory.GetFont("Tahoma", 8f, 1);
+            _pdfCell = new PdfPCell(new Phrase("Totaal incl. BTW: ", _fontstyle));
+            _pdfCell.Colspan = 3;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_RIGHT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _fontstyle = FontFactory.GetFont("Tahoma", 8f, 0);
+            _pdfCell = new PdfPCell(new Phrase("TestInclBTW", _fontstyle));
+            _pdfCell.Colspan = 1;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _pdfCell = new PdfPCell(new Phrase("\n", _fontstyle));
+            _pdfCell.Colspan = 4;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _pdfCell = new PdfPCell(new Phrase("\n", _fontstyle));
+            _pdfCell.Colspan = 4;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_LEFT;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+            _pdfCell = new PdfPCell(new Phrase("Wij verzoeken u vriendelijk bij het afhalen van de bestelde artikelen het verschuldigde bedrag, giraal of chartaal, te betalen.", _fontstyle));
+            _pdfCell.Colspan = 4;
+            _pdfCell.HorizontalAlignment = Element.ALIGN_CENTER;
+            _pdfCell.Border = 0;
+            _pdfCell.BackgroundColor = BaseColor.WHITE;
+            _pdfCell.ExtraParagraphSpace = 0;
+            _pdfTable.AddCell(_pdfCell);
+            _pdfTable.CompleteRow();
+        }
+    }
+}
