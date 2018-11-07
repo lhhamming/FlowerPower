94b02a8ae4e56c936075079b184f736873f2a54e
Luuk Hamming <luuk.hamming@gmail.com>
2018-11-07 14:05:36 +0100

    Inactieve items toegevoegd

---
+++ FlowerPower/Controllers/InactiefArtikelController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FlowerPower.Models;
+
+namespace FlowerPower.Controllers
+{
+    public class InactiefArtikelController : Controller
+    {
+        private DB_A3D6D6_FlowerPowerLuukEntities db = new DB_A3D6D6_FlowerPowerLuukEntities();
+
+        // GET: InactiefArtikel
+        public ActionResult Index()
+        {
+            return View(db.artikels.ToList());
+        }
+
+        // GET: InactiefArtikel/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            artikel artikel = db.artikels.Find(id);
+            if (artikel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(artikel);
+        }
+    }
+}
